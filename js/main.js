518c72bd2d7ef3ef37726dd9d6112d4bc744d36a
Emerson Melo Martins <emerson25xd@gmail.com>
2024-01-10 22:14:39 -0300

    section characters responsive

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@ var swiper = new Swiper(".slide-characters", {
   slidesPerView: 3.2,
   spaceBetween: 19,
   freeMode: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.1,
+    },
+    768: {
+      slidesPerView: 2.2,
+    },
+    991: {
+      slidesPerView: 2.8,
+    },
+  },
 });
